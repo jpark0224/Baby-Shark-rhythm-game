8ee0a20fd6239c8b0bd063fb0f374831cb7c214d
Julie Park <juliepark242@gmail.com>
2022-01-15 15:36:48 +0000

    Wrote a function to judge keydown accuracy Made a combo count Added settings and how to play buttons

--- scripts/app.js
+++ scripts/app.js
@@ -6,13 +6,12 @@ const containerD = document.querySelector(".containerD");
 const containerF = document.querySelector(".containerF");
 const containerJ = document.querySelector(".containerJ");
 const containerK = document.querySelector(".containerK");
-const noteDTimings = [];
-const noteFTimings = [];
-const noteJTimings = [];
-const noteKTimings = [];
 const allNoteTimeouts = [];
 let collisionHeight = 562.5;
 let noteFallSpeed = 1;
+let comboCount = 0;
+const accuracyScreen = document.querySelector(".accuracyScreen");
+const comboScreen = document.querySelector(".comboScreen");
 
 // timer variables
 const timerBody = document.querySelector("#timer");
@@ -20,6 +19,15 @@ const timerScreen = timerBody.querySelector(".screen");
 let timerId = null;
 
 // note timings
+const noteDTimings = [9250, 9750, 11000, 12750, 13500, 13750, 15000, 15750];
+const noteFTimings = [
+  8000, 9000, 9500, 10000, 10750, 13000, 13250, 14000, 14750, 15750,
+];
+const noteJTimings = [
+  7750, 8250, 8750, 10250, 11250, 12000, 12250, 14500, 15250, 15750,
+];
+const noteKTimings = [8500, 10500, 11500, 11750, 12500, 14250, 15500, 15750];
+
 const d1 = 16750;
 const d2 = d1 + 2500;
 const d3 = d2 + 2000;
@@ -66,87 +74,230 @@ for (let i = 0; i < 6; i++) {
   noteKTimings.push(k9 + 8000 * i);
 }
 
-function makeNote(i, container) {
-  const note = document.createElement("img");
-  note.src = "./images/note.png";
-  note.classList.add("note");
-  container.append(note);
+// populate an array of note objects
+const allNotes = [];
+const dNotes = [];
+const fNotes = [];
+const jNotes = [];
+const kNotes = [];
 
-  let position = 0;
+function populateNotes() {
+  for (let i = 0; i < noteDTimings.length; i++) {
+    const dNote = {
+      timingInMs: noteDTimings[i],
+      key: "d",
+      container: containerD,
+      hasBeenHit: false,
+      position: null,
+      element: null,
+    };
+    allNotes.push(dNote);
+    dNotes.push(dNote);
+  }
+  for (let i = 0; i < noteFTimings.length; i++) {
+    const fNote = {
+      timingInMs: noteFTimings[i],
+      key: "f",
+      container: containerF,
+      hasBeenHit: false,
+      position: null,
+      element: null,
+    };
+    allNotes.push(fNote);
+    fNotes.push(fNote);
+  }
+  for (let i = 0; i < noteJTimings.length; i++) {
+    const jNote = {
+      timingInMs: noteJTimings[i],
+      key: "j",
+      container: containerJ,
+      hasBeenHit: false,
+      position: null,
+      element: null,
+    };
+    allNotes.push(jNote);
+    jNotes.push(jNote);
+  }
+  for (let i = 0; i < noteKTimings.length; i++) {
+    const kNote = {
+      timingInMs: noteKTimings[i],
+      key: "k",
+      container: containerK,
+      hasBeenHit: false,
+      position: null,
+      element: null,
+    };
+    allNotes.push(kNote);
+    kNotes.push(kNote);
+  }
+}
 
-  let start, previousTimeStamp;
-  const maxHeight = collisionHeight * 1.2;
+populateNotes();
+
+// make note elements and animations
+function startNoteFalls() {
+  for (let i = 0; i < allNotes.length; i++) {
+    const noteTimeout = setTimeout(() => {
+      makeNote(allNotes[i]);
+    }, allNotes[i].timingInMs - collisionHeight / noteFallSpeed);
+    allNoteTimeouts.push(noteTimeout);
+  }
+}
+
+function makeNote(note) {
+  note.element = document.createElement("img");
+  note.element.src = "./images/note.png";
+  note.element.classList.add("note");
+  note.container.append(note.element);
+
+  let start;
+  const maxHeight = collisionHeight * 1.5;
+
+  // the note should be removed after max height not matter what. If it was already removed, there is no need to do it again.
+  // the note should be removed when it has been hit.
 
   function nextStep(timestamp) {
-    if (position >= maxHeight) {
-      container.removeChild(note);
+    if (note.hasBeenHit) {
+      // stop animating now
+      note.container.removeChild(note.element);
+      note.position = null;
       return;
     }
+    if (note.position >= maxHeight) {
+      miss("MISS");
+      note.container.removeChild(note.element);
+      note.position = null;
+      return;
+    }
+
     if (start === undefined) {
       start = timestamp;
     }
     const elapsed = timestamp - start;
-    if (previousTimeStamp !== timestamp) {
-      position = Math.min(elapsed * noteFallSpeed, maxHeight);
-      note.style.transform = `translateY(${position}px)`;
-      console.log(noteDTimings[i]);
-    }
-    if (elapsed < 2000) {
-      previousTimeStamp = timestamp;
-      window.requestAnimationFrame(nextStep);
-    }
+
+    note.position = Math.min(elapsed * noteFallSpeed, maxHeight);
+    note.element.style.transform = `translateY(${note.position}px)`;
+
+    window.requestAnimationFrame(nextStep);
   }
 
   // Starting the note animation
   window.requestAnimationFrame(nextStep);
 }
 
-function startNoteFalls() {
-  for (let i = 0; i < noteDTimings.length; i++) {
-    const noteTimeout = setTimeout(() => {
-      makeNote(i, containerD);
-    }, noteDTimings[i] - collisionHeight / noteFallSpeed);
-    allNoteTimeouts.push(noteTimeout);
-  }
+// play keys and add effects
+window.addEventListener("keydown", (e) => {
+  const keyDownTiming = parseFloat(timerScreen.innerHTML) * 1000;
 
-  for (let i = 0; i < noteFTimings.length; i++) {
-    const noteTimeout = setTimeout(() => {
-      makeNote(i, containerF);
-    }, noteFTimings[i] - collisionHeight / noteFallSpeed);
-    allNoteTimeouts.push(noteTimeout);
-  }
+  // set up keys
+  const setupKeys = () => {
+    if (e.key === "d" || e.key === "f" || e.key === "j" || e.key === "k") {
+      // set up key audio
+      const audio = document.querySelector(`audio[key="${e.key}"]`);
+      audio.currentTime = 0;
+      audio.play();
 
-  for (let i = 0; i < noteJTimings.length; i++) {
-    const noteTimeout = setTimeout(() => {
-      makeNote(i, containerJ);
-    }, noteJTimings[i] - collisionHeight / noteFallSpeed);
-    allNoteTimeouts.push(noteTimeout);
-  }
+      // mute keys in intro
+      if (keyDownTiming < d1 - 500) {
+        audio.muted = true;
+      } else {
+        audio.muted = false;
+      }
 
-  for (let i = 0; i < noteKTimings.length; i++) {
-    const noteTimeout = setTimeout(() => {
-      makeNote(i, containerK);
-    }, noteKTimings[i] - collisionHeight / noteFallSpeed);
-    allNoteTimeouts.push(noteTimeout);
-  }
+      // set up keydown effect
+      const key = document.querySelector(`.key[key="${e.key}"]`);
+      key.classList.add("playing");
+    }
+  };
+  setupKeys();
+
+  // judge accuracy
+  const judge = (keyDownTiming) => {
+    if (e.key === "d" || e.key === "f" || e.key === "j" || e.key === "k") {
+      for (let i = 0; i < allNotes.length; i++) {
+        const timingDiff =
+          Math.abs(keyDownTiming - allNotes[i].timingInMs) / 31.25;
+        if (timingDiff <= 1) {
+          hit("PERFECT", i);
+        } else if (timingDiff <= 2) {
+          hit("GOOD", i);
+        } else if (timingDiff <= 3) {
+          hit("BAD", i);
+        }
+      }
+    }
+  };
+
+  judge(keyDownTiming);
+});
+
+function hit(judgment, i) {
+  accuracyScreen.innerHTML = judgment;
+  console.log(judgment);
+  comboCount++;
+  comboScreen.innerHTML = comboCount;
+  allNotes[i].hasBeenHit = true;
 }
 
-// play keys and add effects
-window.addEventListener("keydown", function (e) {
-  const audio = document.querySelector(`audio[key="${e.key}"]`);
-  const key = document.querySelector(`.key[key="${e.key}"]`);
-  audio.currentTime = 0;
-  audio.play();
-  key.classList.add("playing");
+function miss(judgment) {
+  accuracyScreen.innerHTML = judgment;
+  console.log(judgment);
+  comboCount = 0;
+  comboScreen.innerHTML = comboCount;
+}
+
+window.addEventListener("keyup", (e) => {
+  if (e.key === "d" || e.key === "f" || e.key === "j" || e.key === "k") {
+    const key = document.querySelector(`.key[key="${e.key}"]`);
+    key.classList.remove("playing");
+  }
 });
 
-window.addEventListener("keyup", function (e) {
-  const key = document.querySelector(`.key[key="${e.key}"]`);
-  key.classList.remove("playing");
+// instruction
+const openModalButtons = document.querySelectorAll("[data-modal-target]");
+const closeModalButtons = document.querySelectorAll("[data-close-button]");
+const overlay = document.querySelector("#overlay");
+const modal = document.querySelector("#instruction");
+
+openModalButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    openModal(modal);
+  });
 });
+
+overlay.addEventListener("click", () => {
+  const modals = document.querySelectorAll(".modal.active");
+  modals.forEach((modal) => {
+    closeModal(modal);
+  });
+});
+
+closeModalButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const modal = button.closest(".modal");
+    closeModal(modal);
+  });
+});
+
+function openModal(modal) {
+  if (modal == null) {
+    return;
+  }
+  modal.classList.add("active");
+  overlay.classList.add("active");
+}
+
+function closeModal(modal) {
+  if (modal == nul) {
+    return;
+  }
+  modal.classList.remove("active");
+  overlay.classList.remove("active");
+}
 
 // timer
 let startTime = 0;
+timerScreen.innerHTML = `${0 + "s"}`;
 
 function handleStartTimer() {
   if (timerId) {
@@ -173,6 +324,18 @@ function handleResetTimer() {
 
 // start and stop game
 function startGame() {
+  if (timerId) {
+    clearInterval(timerId);
+    timerId = null;
+    startTime = null;
+    allNoteTimeouts.forEach((note) => clearTimeout(note));
+    handleResetTimer();
+    accuracyScreen.innerHTML = null;
+    comboScreen.innerHTML = null;
+  }
+  elapsedTime = 0;
+  timerScreen.innerHTML = `${0 + "s"}`;
+  backgroundAudio.currentTime = 0;
   backgroundAudio.play();
   startNoteFalls();
   handleStartTimer();
@@ -183,6 +346,10 @@ function stopGame() {
   backgroundAudio.currentTime = 0;
   allNoteTimeouts.forEach((note) => clearTimeout(note));
   handleResetTimer();
+  comboCount = 0;
+  accuracyScreen.innerHTML = null;
+  comboScreen.innerHTML = null;
+  allNotes.forEach((note) => (note.hasBeenHit = false));
 }
 
 startButton.addEventListener("click", startGame);
